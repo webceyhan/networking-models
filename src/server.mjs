2dce338fcfca67187e4bca6142b27836bd5c27a7
Evren Ceyhan <evren@ceyhan.io>
2020-10-01 15:24:43 +0200

    done

--- src/server.mjs
+++ src/server.mjs
@@ -1,3 +1,16 @@
-export function initServer() {
-    console.log('hello from server');
+import http from "http";
+
+export function initServer(port = 5000) {
+    const server = http.createServer((request, response) => {
+        // create test data
+        const body = "hello client!";
+
+        // send response body and end
+        console.info("server: responding to request");
+        response.end(body);
+    });
+
+    server.listen(port, () => {
+        console.info("server: listening on port " + port);
+    });
 }
